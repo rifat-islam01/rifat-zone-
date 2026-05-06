37d693d6b1535d9e7f4cd083caa3d3a0bf8649f7
EdgeOne Pages <noreply@edgeone.com>
2026-05-06 17:17:08 +0800

    feat: init

---
+++ src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+import i18nextConfig from '../../next-i18next.config.js';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    const currentLocale = this.props.__NEXT_DATA__.query.locale || i18nextConfig.i18n.defaultLocale;
+  return (
+    <Html lang={currentLocale as string}>
+      <Head>
+        <title>Tech Company Website Template</title>
+      </Head>
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+}
+
+export default MyDocument;
